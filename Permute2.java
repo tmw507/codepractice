a48f69f6a4473dee0d17a7df79a05a3355d547e0
Mingwang Tang <mtang@yahoo-inc.com>
2016-02-03 08:07:56 -0800

    fix find duplicate start value bug

--- Permute2.java
+++ Permute2.java
@@ -18,12 +18,21 @@ public class Permute2 {
         }
         //pick up a position to swap with start and do permutation on [start+1,n)
         for(int i=start; i<nums.length; i++){
-            if(i>start && nums[i]==nums[start] ) continue;
+            //if(i>start && nums[i]==nums[start] ) continue;
+	    if(dupsolution(nums, start, i)) continue;
             System.out.println("start:"+start+" i:"+i);
             swap(nums, start, i);
             permuteHelper(ret, nums, start+1);
             swap(nums, start, i);
         }
+    }
+    boolean dupsolution(int[] nums,int step,int end){
+        //if in range [index,j),there exists number equals num[j],the swap with j is a dupsolution,we swap them early.so we pass by
+	for(int i=step; i<end; ++i){
+	    if(nums[i] == nums[end])
+		return true;
+	}
+	return false;
     }
     private void swap(int[] nums, int i, int j){
             int temp = nums[i];
